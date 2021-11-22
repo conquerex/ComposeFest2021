864b6e67f00a6982ad9ca0cc25ce7c51ec662b5d
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-11-22 22:23:34 +0900

    5. Reusing composables

--- MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
+++ MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
@@ -3,11 +3,14 @@ package what.the.mycodelab.week1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import what.the.mycodelab.week1.ui.theme.MyCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +31,16 @@ class MainActivity : ComponentActivity() {
 /**
  * Composable function 에는 TextView 대신 사용할 수 있는
  * Text 같은 Composavle function들을 넣을 수 있다
+ * .
+ * 4. Tweaking the UI
+ * - Surface
+ * - Modifier
  */
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+private fun Greeting(name: String) {
+  Surface(color = MaterialTheme.colors.primary) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+  }
 }
 
 /**
@@ -39,8 +48,20 @@ fun Greeting(name: String) {
  */
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
   MyCodelabTheme {
+    MyApp()
+  }
+}
+
+/**
+ * 5. Reusing composables
+ * - @Composable 어노테이션을 추가하여 함수를 분리하거나 독립적으로 편집이 가능
+ * - 마치 xml 에서 <include> 를 사용했던 것처럼
+ */
+@Composable
+private fun MyApp() {
+  Surface(color = MaterialTheme.colors.background) {
     Greeting("Android")
   }
 }
