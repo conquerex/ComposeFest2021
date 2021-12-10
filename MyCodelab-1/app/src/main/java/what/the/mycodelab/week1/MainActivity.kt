2c386f92ef139bdfee9414c772decd81221d5116
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-12-10 16:08:04 +0900

    7. State in Compose

--- MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
+++ MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,10 +24,10 @@ class MainActivity : ComponentActivity() {
     // setContent 안에 xml 대신 레이아웃을 구성할 Composable 함수가 들어갈 수 있다
     setContent {
       MyCodelabTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
+//        Surface(color = MaterialTheme.colors.background) {
+//          Greeting("Android")
+//        }
+        MyApp()
       }
     }
   }
@@ -41,6 +43,9 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 private fun Greeting(name: String) {
+  val expanded = remember { mutableStateOf(false) }
+  val extraPadding = if (expanded.value) 48.dp else 0.dp
+
   Surface(
     color = MaterialTheme.colors.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -52,14 +57,17 @@ private fun Greeting(name: String) {
 //      Text(text = name)
 //    }
     Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier.weight(1f)) {
+      Column(modifier = Modifier
+        .weight(1f)
+        .padding(bottom = extraPadding)
+      ) {
         Text(text = "Hello, ")
         Text(text = name)
       }
       OutlinedButton(
-        onClick = { /* TODO */ }
+        onClick = { expanded.value = !expanded.value }
       ) {
-        Text("Show more")
+        Text(if (expanded.value) "Show less" else "Show more")
       }
     }
   }
