62121a3fe57d67abe7064eeef3a8fc7be868c5d3
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-12-10 16:30:14 +0900

    12. Styling and theming your app

--- MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
+++ MyCodelab-1/app/src/main/java/what/the/mycodelab/week1/MainActivity.kt
@@ -1,5 +1,6 @@
 package what.the.mycodelab.week1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import what.the.mycodelab.week1.ui.theme.MyCodelabTheme
@@ -58,13 +60,14 @@ private fun Greeting(name: String) {
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
   ) {
     Row(modifier = Modifier.padding(24.dp)) {
-      Column(
-        modifier = Modifier
-          .weight(1f)
-          .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-      ) {
+      Column(modifier = Modifier.weight(1f)) {
         Text(text = "Hello, ")
-        Text(text = name)
+        Text(
+          text = name,
+          style = MaterialTheme.typography.h4.copy(
+            fontWeight = FontWeight.ExtraBold
+          )
+        )
       }
       OutlinedButton(
         onClick = { expanded = !expanded }
@@ -80,11 +83,17 @@ private fun Greeting(name: String) {
 /**
  * @Preview 를 통해 다양한 프리뷰를 볼 수 있다.
  */
+@Preview(
+  showBackground = true,
+  widthDp = 320,
+  uiMode = UI_MODE_NIGHT_YES,
+  name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun DefaultPreview() {
   MyCodelabTheme {
-    MyApp()
+    Greetings()
   }
 }
 
