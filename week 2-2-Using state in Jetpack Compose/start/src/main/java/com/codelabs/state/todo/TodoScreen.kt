85d6644c9a95d2c5ee1b20af4636478718ce9583
Conquerex <conquerex@gmail.com>
2021-12-10 19:36:44 +0900

    6. State in Compose

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -25,7 +25,9 @@ import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,6 +48,10 @@ fun TodoScreen(
   onAddItem: (TodoItem) -> Unit,
   onRemoveItem: (TodoItem) -> Unit
 ) {
+  TodoItemInputBackground(elevate = true, modifier = Modifier.fillMaxWidth()) {
+    TodoItemInput(onItemComplete = onAddItem)
+  }
+
   Column {
     LazyColumn(
       modifier = Modifier.weight(1f),
@@ -60,7 +66,6 @@ fun TodoScreen(
       }
     }
 
-    // For quick testing, a random item generator button
     Button(
       onClick = { onAddItem(generateRandomTodoItem()) },
       modifier = Modifier
@@ -123,4 +128,38 @@ fun PreviewTodoScreen() {
 fun PreviewTodoRow() {
   val todo = remember { generateRandomTodoItem() }
   TodoRow(todo = todo, onItemClicked = {}, modifier = Modifier.fillMaxWidth())
+}
+
+@Composable
+fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: Modifier) {
+  TodoInputText(text, onTextChange, modifier)
+}
+
+@Composable
+fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
+  val (text, setText) = remember { mutableStateOf("") }
+  Column {
+    Row(
+      Modifier
+        .padding(horizontal = 16.dp)
+        .padding(top = 16.dp)
+    ) {
+      TodoInputText(
+        text = text,
+        onTextChange = setText,
+        modifier = Modifier
+          .weight(1f)
+          .padding(end = 8.dp)
+      )
+      TodoEditButton(
+        onClick = {
+          onItemComplete(TodoItem(text))
+          setText("")
+        },
+        text = "Add",
+        modifier = Modifier.align(Alignment.CenterVertically),
+        enabled = text.isNotBlank()
+      )
+    }
+  }
 }
